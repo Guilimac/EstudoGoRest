d119edee97dcba902c67e9d3492487994d953549
Guilherme de Lima Conceição <guilherme.conceicao@maltacobrancas.com.br>
2018-03-13 18:34:36 -0300

    refactory para divisão do projeto

---
+++ handlers.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
+
+}
